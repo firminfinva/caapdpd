5d413d7d681cd76b14f54b2bf120d73c678d9ff2
firmin nagnduli <finganduli@gmail.com>
2024-03-16 00:19:21 +0200

    added the footer , the aprops page and the number animation on the home page

--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home/home.html', {})
+
+def apropos(request):
+    return render(request, 'home/apropos.html', {})
